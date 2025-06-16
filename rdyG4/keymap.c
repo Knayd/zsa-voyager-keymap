fe4142d43043f92fc454277bdf5958f9b443a3c7
Deryan Argueta <deryan.hndz@gmail.com>
2025-06-15 19:51:27 -0600

    use fn as tri layer

--- rdyG4/keymap.c
+++ rdyG4/keymap.c
@@ -235,6 +235,10 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   return true;
 }
 
+layer_state_t layer_state_set_user(layer_state_t state) {
+  return update_tri_layer_state(state, 1, 2, 3);
+}
+
 typedef struct {
     bool is_press_action;
     uint8_t step;
