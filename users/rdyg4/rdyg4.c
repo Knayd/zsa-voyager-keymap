e05b60ffc48b2f01b6f333295a9cf83d70cc9f4b
Deryan Argueta <deryan.hndz@gmail.com>
2025-12-18 03:00:03 -0600

    chore: update userspace file names

---
+++ users/rdyg4/rdyg4.c
@@ -0,0 +1,12 @@
+#include "rdyg4.h"
+#include "magickey.h"
+#include "os_mode.h"
+
+__attribute__ ((weak))
+bool process_record_keymap(uint16_t keycode, keyrecord_t *record) {
+  return true;
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    return process_record_keymap(keycode, record);
+}
